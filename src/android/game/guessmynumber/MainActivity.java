ad63ef9bc17ffc0ea65758e7fc764cc32ff991f1
Rushabh Padalia <padalia.rushabh@gmail.com>
2013-08-10 01:21:12 +0800

    initial conroller logic

--- src/android/game/guessmynumber/MainActivity.java
+++ src/android/game/guessmynumber/MainActivity.java
@@ -2,7 +2,11 @@ package android.game.guessmynumber;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -12,11 +16,31 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
+	/**On setting clicked open settingActivity**/
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		// TODO Auto-generated method stub
+		switch(item.getItemId()){
+		case R.id.menu_settings:
+			Intent intent = new Intent(this , SettingActivity.class);
+			startActivity(intent);
+			break;
+		}
+		return super.onMenuItemSelected(featureId, item);
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		//connect the menu to res/menu/settings.xml
+		getMenuInflater().inflate(R.menu.settings, menu);
 		return true;
+	}
+	
+	/**On start button clicked**/
+	public void startGame(View v){
+		
+		Intent intent = new Intent(MainActivity.this, GameActivity.class);
+		startActivity(intent);
 	}
 
 }
---
+++ src/android/game/guessmynumber/SettingActivity.java
@@ -0,0 +1,33 @@
+package android.game.guessmynumber;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SettingActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_setting);
+	}
+	
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		// TODO Auto-generated method stub
+		return super.onMenuItemSelected(featureId, item);
+	}
+	
+	@Override
+	/**Diaplay back button only**/
+	public boolean onCreateOptionsMenu(Menu menu) {
+		
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		return true;
+	}
+}
