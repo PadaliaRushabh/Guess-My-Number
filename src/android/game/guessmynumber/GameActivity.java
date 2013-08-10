12c1cd305b6e012eeab898e305c20b73354cac7b
Rushabh Padalia <padalia.rushabh@gmail.com>
2013-08-10 16:14:21 +0800

    gride view on game activity

--- src/android/game/guessmynumber/GameActivity.java
+++ src/android/game/guessmynumber/GameActivity.java
@@ -3,12 +3,31 @@ package android.game.guessmynumber;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 public class GameActivity extends Activity {
+	
+	GridView gridView;
+	
+	static final String[] numbers = new String[] { 
+		"A", "B", "C", "D", "E",
+		"F", "G", "H", "I", "J",
+		"K", "L", "M", "N", "O",
+		"P", "Q", "R", "S", "T",
+		"U", "V", "W", "X", "Y", "Z"};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
+		
+		gridView = (GridView) findViewById(R.id.gridViewNumbers);
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, numbers);
+		
+		gridView.setAdapter(adapter);
 	}
 
 	@Override
