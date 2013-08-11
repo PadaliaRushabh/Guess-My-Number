6b23d60020f087548c7e03dfc2e8bd1a7adb98da
Rushabh Padalia <padalia.rushabh@gmail.com>
2013-08-11 22:43:04 +0800

    class to start, pause and resume background music

---
+++ src/android/game/guessmynumber/MusicHelper.java
@@ -0,0 +1,68 @@
+package android.game.guessmynumber;
+
+public class MusicHelper {
+	
+	private static final int MAX_ACTIVITY = 4;
+	private static int visible = 0;
+	private static int paused = 0;
+	private static int destroyed = 0;
+	
+	public static void manipulateDestroyed(int operation){
+		switch(operation){
+		case 0:
+			destroyed = destroyed - 1;
+			break;
+		case 1:
+			destroyed = destroyed + 1;
+			break;
+		}
+		isVisible();
+	}
+	
+	public static void manipulateVisible(int operation){
+		if(visible > 0){
+			switch(operation){
+			case 0:
+				visible = visible - 1;
+				break;
+			case 1:
+				visible = visible + 1;
+				break;
+			}
+		}
+	}
+	
+	public static void manipulatePause(int operation){
+		if(paused > 0){
+			switch(operation){
+			case 0:
+				paused = paused - 1;
+				break;
+			case 1:
+				paused = paused + 1;
+				break;
+			}
+		}
+	}
+	
+	public static int getPause(){
+		return paused;
+	}
+	
+	public static int getVisible(){
+		return visible;
+	}
+	
+	public static int getDestroyed(){
+		return destroyed;
+	}
+	
+	public static boolean isVisible(){
+		
+		if (visible == 0){
+			return false;
+		}
+			return true;
+	}
+	
+}
